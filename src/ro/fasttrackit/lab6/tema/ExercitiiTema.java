d32ade13eeca375f9d3458c0e87b5c1f1b82c1e0
StefaniaCrisan <stefania.vlaic@yahoo.es>
2022-03-26 17:39:10 +0200

    Exercitii tema lab 6

--- src/ro/fasttrackit/lab6/tema/ExercitiiTema.java
+++ src/ro/fasttrackit/lab6/tema/ExercitiiTema.java
@@ -1,7 +1,17 @@
 package ro.fasttrackit.lab6.tema;
 
+import java.util.Arrays;
+
 public class ExercitiiTema {
     public static void main(String[] args) {
+        System.out.println(produsIntregiSir(new int[]{1, 22, 3, 43, 5, 61, 7}));
+    }
 
+    private static int produsIntregiSir(int[] sir) {
+        int rezultat = 1;
+        for (int i = 0; i < sir.length; i++) {
+            rezultat *= sir[i];
+        }
+        return rezultat;
     }
 }
