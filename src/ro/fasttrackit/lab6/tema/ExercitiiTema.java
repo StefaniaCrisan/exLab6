e0efb816efb0b5bb297c5400f346faeae5876d5b
StefaniaCrisan <stefania.vlaic@yahoo.es>
2022-03-27 18:44:59 +0300

    Exercitii tema lab 6

--- src/ro/fasttrackit/lab6/tema/ExercitiiTema.java
+++ src/ro/fasttrackit/lab6/tema/ExercitiiTema.java
@@ -4,7 +4,32 @@ import java.util.Arrays;
 
 public class ExercitiiTema {
     public static void main(String[] args) {
-        System.out.println(produsIntregiSir(new int[]{1, 22, 3, 43, 5, 61, 7}));
+        System.out.println("1. Produsul numerelor: " + produsIntregiSir(new int[]{1, 22, 3, 43, 5, 61, 7}));
+        System.out.println(numereImpare(new int[]{1, 2, 3, 4, 5, 6, 7}));
+        System.out.println(numereMaiMici(new int[]{1, 2, 3, 4, 5, 6, 7}, 5));
+    }
+
+    private static int numereMaiMici(int[] sir, int numar) {
+        System.out.println("3. Numerele mai mici decat " + numar + " sunt:");
+        int i = 0;
+        for (i = 0; i < sir.length; i++) {
+            if (sir[i] < numar) {
+                System.out.println(sir[i]);
+            }
+        }
+        return numar;
+    }
+
+    private static int[] numereImpare(int[] sir) {
+        System.out.println("2. Numarele impare sunt: ");
+        int[] sirImpar = new int[sir.length];
+        for (int i = 0; i < sir.length; i++) {
+            if (sir[i] % 2 != 0) {
+                sirImpar[i] = sir[i];
+                System.out.println(sirImpar[i]);
+            }
+        }
+        return sirImpar;
     }
 
     private static int produsIntregiSir(int[] sir) {
@@ -14,4 +39,5 @@ public class ExercitiiTema {
         }
         return rezultat;
     }
+
 }
