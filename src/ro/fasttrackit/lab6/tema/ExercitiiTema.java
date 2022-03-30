873d05de82127d2c6c4dc87dd76df96c719de326
StefaniaCrisan <stefania.vlaic@yahoo.es>
2022-03-30 14:01:11 +0300

    Exercitii tema lab 6 Coding: more practice

--- src/ro/fasttrackit/lab6/tema/ExercitiiTema.java
+++ src/ro/fasttrackit/lab6/tema/ExercitiiTema.java
@@ -1,12 +1,39 @@
 package ro.fasttrackit.lab6.tema;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.lang.String;
+import java.util.regex.Pattern;
+
 
 public class ExercitiiTema {
     public static void main(String[] args) {
         System.out.println("1. Produsul numerelor: " + produsIntregiSir(new int[]{1, 22, 3, 43, 5, 61, 7}));
         System.out.println(numereImpare(new int[]{1, 2, 3, 4, 5, 6, 7}));
         System.out.println(numereMaiMici(new int[]{1, 2, 3, 4, 5, 6, 7}, 5));
+        System.out.println("4. Donatiile sunt: " + donatii(10000));
+        separare("Tema lab 6. Fasttrackit. Curs Java");
+    }
+
+    private static String separare(String fraza) {
+        System.out.println("Propozitiile sunt: ");
+        String[] arrOfStr = fraza.split(Pattern.quote(". "));
+        for (String a : arrOfStr)
+            System.out.println(a);
+        return fraza;
+    }
+
+    private static int donatii(int target) {
+        int donatie=0;
+      int[] sir=new int[10];
+        Random random = new Random();
+        for (int i = 0; i < sir.length; i++) {
+                donatie = random.nextInt(target-donatie);
+                System.out.println(donatie);
+                sir[i]=donatie;
+
+        }
+        return donatie;
     }
 
     private static int numereMaiMici(int[] sir, int numar) {
